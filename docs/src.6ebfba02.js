b70d7ec4aa7a69873267fd501bd54e14a8db4745
Chris <59393694+UKChris-osm@users.noreply.github.com>
2021-02-25 12:35:50 +0000

    npm run appbuild

--- docs/src.6ebfba02.js
+++ docs/src.6ebfba02.js
@@ -12220,7 +12220,7 @@ function buildOverpassTurbo(itemData) {
   var OverpassTurboQueryURI = encodeURIComponent(OverpassTurboQuery);
   console.log(OverpassTurboQueryURI);
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("pre", null, OverpassTurboQuery)), /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("a", {
-    href: "https://overpass-turbo.eu/?Q={OverpassTurboQueryURI}&R"
+    href: "https://overpass-turbo.eu/?Q={{OverpassTurboQueryURI}}&R"
   }, "Try query")), /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("pre", null, OverpassTurboQuery)), /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("pre", null, OverpassTurboQueryURI)));
 }
 },{"react":"n8MK","react-router-dom":"uc19","./CategoryRowSocialLinks":"GZ5J"}],"lmHt":[function(require,module,exports) {
