e9ea9577af6df5e0101e598bec4b0adb04489d39
Chris <59393694+UKChris-osm@users.noreply.github.com>
2021-04-16 18:25:50 +0100

    date

--- app/src/Footer.jsx
+++ app/src/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer(props) {
   const generated = meta && meta.generated;
   const released = generated && new Date(Date.parse(generated));
 
-  const trimmed = released.getFullYear();
+  const trimmed = generated.getFullYear();
   const display = released && version && `NSI v${version} (Generated: ${released} [${trimmed}])`;
   return (
     <div id='footer'>{display}</div>
--- docs/src.6ebfba02.js
+++ docs/src.6ebfba02.js
@@ -19953,7 +19953,7 @@ function Footer(props) {
   var version = meta && meta.version;
   var generated = meta && meta.generated;
   var released = generated && new Date(Date.parse(generated));
-  var trimmed = released.getFullYear();
+  var trimmed = generated.getFullYear();
   var display = released && version && "NSI v".concat(version, " (Generated: ").concat(released, " [").concat(trimmed, "])");
   return /*#__PURE__*/_react.default.createElement("div", {
     id: "footer"
